5ae5db90f54ae36b156b62e03db8f875b8be72c2
ynx-desktop <yunongxin@sina.com>
2020-01-14 21:24:44 +0800

    [Add] 学习并发
    1. 弹球Demo，未使用多线程。

---
+++ bounce/BallComponent.java
@@ -0,0 +1,40 @@
+package bounce;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yunongxin
+ * @version V1.0
+ * @date 2020-01-14
+ * @modified_date 2020-01-14
+ */
+public class BallComponent extends JPanel {
+    private static final int DEFAULT_WIDTH = 450;
+    private static final int DEFAULT_HEIGHT = 350;
+
+    private List<Ball> balls = new ArrayList<>();
+
+    /**
+     * Add a ball to the component.
+     * @param b the ball to add
+     */
+    public void add(Ball b) {
+        balls.add(b);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        for (Ball b : balls) {
+            g2.fill(b.getShape());
+        }
+    }
+
+    public Dimension getPerferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
