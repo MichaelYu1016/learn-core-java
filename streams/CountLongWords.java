d2bbd0cf0bc97b2b0372ba860771a2496dee5569
ynx-desktop <yunongxin@sina.com>
2020-04-07 21:48:21 +0800

    [Add] Streams

---
+++ streams/CountLongWords.java
@@ -0,0 +1,34 @@
+package streams;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 流操作和迭代操作
+ * @author ynx
+ * @version V1.0
+ * @date 2020-04-07
+ */
+public class CountLongWords {
+    public static void main(String[] args) throws IOException {
+        String contents = new String(Files.readAllBytes(Paths.get("F:\\alice19033.txt")), StandardCharsets.UTF_8);
+
+        List<String> words = Arrays.asList(contents.split("\\PL+"));
+
+        long count = 0;
+        for (String w : words) {
+            if (w.length() > 12) count++;
+        }
+        System.out.println(count);
+
+        count = words.stream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+
+        count = words.parallelStream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+    }
+}
